11a62926d63261d4c718105a37b1c5508fe4b2e7
Dinh Thanh An <thanhan.uit@gmail.com>
2017-03-15 11:56:49 +0700

    finish client feature

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,20 +1,31 @@
 import React from 'react';
-
-import {
-  View,
-  Text,
-} from 'react-native';
+import {GiftedChat} from 'react-native-gifted-chat';
 
 class Chat extends React.Component {
+  state = {
+    messages: []
+  };
   render() {
     return (
-      <View>
-        <Text>
-          Hello Chat
-        </Text>
-      </View>
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={(message) => {
+
+        }}
+        user={{
+          _id: 1,
+        }}
+      />
     );
   }
 }
+
+Chat.defaultProps = {
+  name: 'An',
+};
+
+Chat.propTypes = {
+  name: React.PropTypes.string,
+};
 
 export default Chat;
